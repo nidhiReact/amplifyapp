7a33eeef62cd8047a6dcfe03f80dc8f535669d89
nidhiReact <nidhigirish20@gmail.com>
2020-12-09 17:50:30 +0530

    new css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { withAuthenticator } from "aws-amplify-react";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
+import 'bootstrap/dist/css/bootstrap.min.css';
 Amplify.configure(aws_exports);
 
 class App extends Component {
